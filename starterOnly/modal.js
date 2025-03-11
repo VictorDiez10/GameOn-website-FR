4f6ae84ebfd697ecb40eaced84879135bf5fb1d7
Victor Diez <victordiez1@gmail.com>
2025-03-11 14:33:02 +0100

    Close modal, implémenter entrées du formulaire, ajouter validiation ou messages d'erreur

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -10,7 +10,6 @@ function editNav() {
 // DOM Elements
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
-const formData = document.querySelectorAll(".formData");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -20,4 +19,98 @@ function launchModal() {
   modalbg.style.display = "block";
 }
 
+const modalClose = document.querySelectorAll(".close");
+modalClose.forEach((close) => close.addEventListener("click", closeModal))
 
+function closeModal() {
+  modalbg.style.display = "none"
+}
+
+
+//Pointer les differents champs du form
+
+const firstName = document.getElementById("first");
+const last = document.getElementById("last");
+const email = document.getElementById("email");
+const birthDate = document.getElementById("birthdate");
+const quantity = document.getElementById("quantity");
+const newYork = document.getElementById("location1");
+const sanFrancisco = document.getElementById("location2");
+const seattle = document.getElementById("location3");
+const chicago = document.getElementById("location4");
+const boston = document.getElementById("location5");
+const portLand = document.getElementById("location6");
+const checkbox1 = document.getElementById("checkbox1");
+const checkbox2 = document.getElementById("checkbox2");
+const submit = document.querySelectorAll(".btn-submit")
+const form = document.getElementById("form");
+
+
+
+form.addEventListener("submit", event => {
+  event.preventDefault();
+  validate();
+})
+
+
+function validate () {
+  if(firstName.value.length < 2 || firstName.value === null || firstName.value === "") {
+    setError(firstName, "Veuillez entrer 2 caractères ou plus pour le champ du Prénom.")
+} else {
+  setSuccess(firstName)
+}
+
+  if(last.value.length < 2 || last.value === null || last.value === '') {
+    setError(last, "Veuillez entrer 2 caractères ou plus pour le champ du nom.")
+  } else {
+    setSuccess(last)
+  }
+
+const isEmail = /^((?!\.)[\w-_.]*[^.])(@\w+)(\.\w+(\.\w+)?[^.\W])$/gim;
+  if (!isEmail.test(email.value) || email.value === null || email.value === "") {
+    setError(email, "Entrez une adresse mail valide")
+  } else {
+    setSuccess(email)
+  }
+
+const isdate = /^[0-9]{1,2}\/[0-9]{1,2}\/[0-9]{4}$/;
+if(!birthDate.value.match(isdate) || birthDate.value === null || birthDate.value === "") {
+  setError(birthDate, "Vous devez entrer votre date de naissance.")
+} else {
+  setSuccess(birthDate)
+}
+
+if(!quantity === Number || quantity.value === null || quantity.value === "") {
+  setError(quantity, "Une valeur numérique doit être saisie.")
+} else {
+  setSuccess(quantity)
+}
+
+console.log(!newYork.checked && !sanFrancisco.checked && !seattle.checked && !chicago.checked && !boston.checked && !portLand.checked)
+
+if (!newYork.checked && !sanFrancisco.checked && !seattle.checked && !chicago.checked && !boston.checked && !portLand.checked) {
+  setError(newYork, "Vous devez choisir une ville.")
+} else {
+  setSuccess(newYork)
+}
+
+if(!checkbox1.checked) {
+  setError(checkbox1, "Vous devez vérifier que vous acceptez les termes et conditions.")
+} else {
+  setSuccess(checkbox1)
+}
+  
+} 
+
+const setError = (input, message) => {
+  const formData = input.parentElement;
+	const small = formData.querySelector('small');
+	formData.className = 'formData error';
+	small.innerText = message;
+}
+
+function setSuccess(input) {
+	const formData = input.parentElement;
+  const small = formData.querySelector("small")
+	formData.className = 'formData success';
+}
